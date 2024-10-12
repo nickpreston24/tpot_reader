d8877684eec3952995aa874ca3f44e6891ade675
nickpreston24 <nicholas.preston24@gmail.com>
2024-10-12 17:33:54 -0500

    ++ libman, sqlite and basic razorhat support

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,6 @@
+using CodeMechanic.Diagnostics;
+using CodeMechanic.FileSystem;
+using CodeMechanic.Types;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,8 +15,47 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
-    public void OnGet()
-    {
+    public void OnGet() { }
 
+    private async Task<List<string>> GetAllLocalTpotLinks()
+    {
+        // string sample_link = @"https://www.thepathoftruth.com/teachings/on-gifts-tithes-and-offerings-to-thepathoftruth";
+        string url_regex = @"(?<http_type>https?)://www\.(thepathoftruth.com)(?<remainder>.*?)$";
+        // https://regex101.com/r/jDfmhS/1
+
+        string desktop_dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        Console.WriteLine("desktop :>> " + desktop_dir);
+
+        var sharpifyrc_files = new Grepper()
+        {
+            RootPath = desktop_dir,
+            Recursive = true,
+            FileSearchLinePattern = url_regex
+        }
+            .GetMatchingFiles()
+            .ToList();
+
+            sharpifyrc_files.Dump("files");
+
+
+        return new List<string>();
+    }
+
+    public async Task<IActionResult> OnGetMarkAsRed(string id = "", string paper_url = "")
+    {
+        var links_only = await GetAllLocalTpotLinks();
+        links_only.Dump();
+        return Content("Update complete.");
     }
 }
+
+public class TpotUrl
+{
+    public string http_type { get; set; } = string.Empty;
+    public string remainder { get; set; } = string.Empty;
+}
+
+// public class TpotLinksRegex : Enumeration {
+
+
+// }
--- Resources.cs
+++ Resources.cs
@@ -8,8 +8,7 @@ public static class Resources
 {
     private static string[] resources;
 
-    public static Assembly ThisAssembly
-        => typeof(Resources).Assembly;
+    public static Assembly ThisAssembly => typeof(Resources).Assembly;
 
     public static void LoadResources(this Assembly? assembly, bool debug = false)
     {
@@ -20,11 +19,13 @@ public static class Resources
         }
 
         resources = assembly.GetManifestResourceNames();
-        if (debug) Console.WriteLine("total resources found: " + resources.Length);
+        if (debug)
+            Console.WriteLine("total resources found: " + resources.Length);
         if (resources.Length == 0)
             return;
 
-        if (debug) Console.WriteLine($"Resources in {assembly.FullName}");
+        if (debug)
+            Console.WriteLine($"Resources in {assembly.FullName}");
         if (debug)
             foreach (var resource in resources)
                 Console.WriteLine(resource);
@@ -37,17 +38,16 @@ public static class Resources
 
         public static string GetFile(string filename, bool debug = false)
         {
-            if (filename.IsEmpty()) throw new ArgumentNullException(nameof(filename));
+            if (filename.IsEmpty())
+                throw new ArgumentNullException(nameof(filename));
             // if (debug) Console.WriteLine($"filname :>> {filename}");
             if (!Path.HasExtension(filename))
-                throw new ArgumentException(nameof(filename) +
-                                            " must have an extension!"); // do NOT allow extensionless files!
+                throw new ArgumentException(nameof(filename) + " must have an extension!"); // do NOT allow extensionless files!
             // if (debug) resources.Dump("current resources");
             string filepath = resources.SingleOrDefault(name => name.ToLower().Contains(filename));
-            if (debug) Console.WriteLine("file path: \n" + filepath);
-            using var stream = Assembly
-                .GetExecutingAssembly()
-                .GetManifestResourceStream(filepath)!;
+            if (debug)
+                Console.WriteLine("file path: \n" + filepath);
+            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(filepath)!;
             using var streamReader = new StreamReader(stream, Encoding.UTF8);
             return streamReader.ReadToEnd();
         }
@@ -60,8 +60,10 @@ public static class Resources
                 // var ass_name = info.Name;
                 // if (debug) Console.WriteLine("ass name:>> " + ass_name);
                 string filename = $"{sproc_name}.sql";
-                string filepath = resources.FirstOrDefault(name => name.ToLower().Contains(filename));
-                // if (debug) 
+                string filepath = resources.FirstOrDefault(name =>
+                    name.ToLower().Contains(filename)
+                );
+                // if (debug)
                 Console.WriteLine("file path: \n" + filepath);
                 using var stream = Assembly
                     .GetExecutingAssembly()
